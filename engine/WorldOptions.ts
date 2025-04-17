ad3739a46f21366d827ee1dd8e1e5cff601600fc
Wallisonsouza <wallisonsouza200@gmail.com>
2025-04-16 23:08:21 -0300

    feat: Implementação básica da cena 3D com carregamento de modelo GLTF, câmera e iluminação
    - Adicionada classe `SimpleScene` para gerenciar a cena do jogo.
    - Implementado método `load` para carregar objetos do modelo GLTF, configurar câmera e iluminação.
    - Criada câmera com controle (`CameraControler`) e luz direcional.
    - Código preparado para futuras funcionalidades, como criação e manipulação de objetos dinâmicos.
    - Removido código comentado não utilizado para melhorar a legibilidade.
    - Adicionada estrutura básica para métodos `start` e `update` para inicialização e atualização da cena.
    
    Melhorias:
    - Organização do código em métodos menores para separação de responsabilidades.

--- engine/WorldOptions.ts
+++ engine/WorldOptions.ts
@@ -5,6 +5,6 @@ export default class WorldOptions {
     public static color: Color  = new Color(0.51, 0.51, 0.51);
     public static strength: number = 1.0;
     public static renderPass: number = 0;
-    public static renderMode: RenderMode = RenderMode.LINES;
+    public static renderMode: RenderMode = RenderMode.TRIANGLES;
     public static environmentTexture: WebGLTexture | null = null;
 }
